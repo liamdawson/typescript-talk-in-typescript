9a8d4fd7a9923fbf4b45facfff7eba14d5ceca32
Liam Dawson <liam@ldaws.com>
2018-09-14 16:41:45 +1000

    add some event listeners

--- src/Presentation/index.tsx
+++ src/Presentation/index.tsx
@@ -1,8 +1,26 @@
 import * as React from 'react';
 
 export class PresentationComponent extends React.Component {
+    private inputHandler: (event: Event) => void;
+
     public render() {
         return <div />;
+    }
+
+    public componentDidMount() {
+        this.inputHandler = this.onDocumentKeyPress.bind(this);
+
+        document.addEventListener('keyup', this.inputHandler);
+        document.addEventListener('mousewheel', this.inputHandler);
+    }
+
+    public componentWillUnmount() {
+        document.removeEventListener('keyup', this.inputHandler);
+        document.removeEventListener('mousewheel', this.inputHandler);
+    }
+
+    private onDocumentKeyPress() {
+
     }
 }
 
