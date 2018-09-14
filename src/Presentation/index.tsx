ff805b35414a95cacc18db220dcae3dca1b9b3bf
Liam Dawson <liam@ldaws.com>
2018-09-14 15:24:33 +1000

    start structure

---
+++ src/Presentation/index.tsx
@@ -0,0 +1,9 @@
+import * as React from 'react';
+
+export class PresentationComponent extends React.Component {
+    public render() {
+        return <div />;
+    }
+}
+
+export default PresentationComponent;
--- src/index.tsx
+++ src/index.tsx
@@ -1,11 +1,8 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
-import App from './App';
-import './index.css';
-import registerServiceWorker from './registerServiceWorker';
+import Presentation from './Presentation';
 
 ReactDOM.render(
-  <App />,
+  <Presentation />,
   document.getElementById('root') as HTMLElement
 );
-registerServiceWorker();
