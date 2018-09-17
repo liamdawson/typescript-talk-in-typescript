b7f6f5c8da4347a44a42d1bd54c8a5e07a840fdd
Liam Dawson <liam@ldaws.com>
2018-09-17 22:38:41 +1000

    enable history-bois

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,5 +1,7 @@
+import { History } from 'history';
 import * as keycode from 'keycode';
 import * as React from 'react';
+import { HashRouter as Router, Redirect, Route, RouteComponentProps, Switch } from 'react-router-dom';
 import Presentation from './Presentation';
 
 const nextKeys = [
@@ -19,35 +21,38 @@ interface IProps {
   slides: JSX.Element[],
 }
 
-interface IState {
-  currentSlide: number,
-}
-
-export class HomeComponent extends React.Component<IProps, IState> {
-  private slideChangeHandler: (_: number, newSlide: number) => void;
-
+export class HomeComponent extends React.Component<IProps> {
   constructor(props: IProps) {
     super(props);
-    this.state = {
-      currentSlide: 1,
-    }
-    this.slideChangeHandler = this.handleSlideChange.bind(this);
   }
 
   public render() {
-    return <Presentation
-      slides={this.props.slides}
-      onSlideChangeRequested={this.slideChangeHandler}
-      displayedSlide={this.state.currentSlide}
-      nextKeyCodes={nextKeys}
-      prevKeyCodes={prevKeys}
-    />;
+    const redirectHome = () => <Redirect to='/' />;
+    const renderPresentation = ((routerProps: RouteComponentProps) => 
+        <Presentation
+          slides={this.props.slides}
+          onSlideChangeRequested={this.slideChangeHandler(routerProps.history)}
+          // tslint:disable-next-line:no-string-literal
+          displayedSlide={parseInt(routerProps.match.params['id'], 10)}
+          nextKeyCodes={nextKeys}
+          prevKeyCodes={prevKeys}
+        />);
+
+    return (<Router>
+      <Switch>
+        <Redirect from='/' exact={true} to='/1' />
+        <Route path='/:id' render={renderPresentation} />
+        <Route render={redirectHome} />
+      </Switch>
+    </Router>);
   }
 
-  private handleSlideChange(_: number, newSlide: number) {
-    this.setState({
-      currentSlide: newSlide,
-    });
+  private slideChangeHandler(history: History) {
+    return (_: number, newSlide: number) => {
+      history.push({
+        pathname: `/${newSlide}`
+      });
+    };
   }
 }
 
