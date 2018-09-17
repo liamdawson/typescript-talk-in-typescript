107dc5ca48b891c55571a947616bc9297625cc01
Liam Dawson <liam@ldaws.com>
2018-09-17 17:14:30 +1000

    add PoC key press handling

--- src/Presentation/index.tsx
+++ src/Presentation/index.tsx
@@ -1,26 +1,53 @@
 import * as React from 'react';
 
-export class PresentationComponent extends React.Component {
+export interface IProps {
+    startingSlide?: number,
+    nextKeyCodes?: number[],
+    prevKeyCodes?: number[],
+}
+
+interface IState {
+    currentSlideNumber: number;
+}
+
+export class PresentationComponent extends React.Component<IProps, IState> {
     private inputHandler: (event: Event) => void;
 
+    constructor(props: IProps) {
+        super(props);
+        this.state = {
+            currentSlideNumber: this.props.startingSlide
+                ? this.props.startingSlide
+                : 1,
+        };
+    }
+
     public render() {
-        return <div />;
+        return <div>
+            {this.state.currentSlideNumber}
+        </div>;
     }
 
     public componentDidMount() {
         this.inputHandler = this.onDocumentKeyPress.bind(this);
 
         document.addEventListener('keyup', this.inputHandler);
-        document.addEventListener('mousewheel', this.inputHandler);
+        // document.addEventListener('mousewheel', this.inputHandler);
     }
 
     public componentWillUnmount() {
         document.removeEventListener('keyup', this.inputHandler);
-        document.removeEventListener('mousewheel', this.inputHandler);
+        // document.removeEventListener('mousewheel', this.inputHandler);
     }
 
-    private onDocumentKeyPress() {
-
+    private onDocumentKeyPress(e: Event) {
+        if (e instanceof KeyboardEvent) {
+            if (this.props.nextKeyCodes && this.props.nextKeyCodes.indexOf(e.keyCode) !== -1) {
+                this.setState({currentSlideNumber: this.state.currentSlideNumber + 1});
+            } else if(this.props.prevKeyCodes && this.props.prevKeyCodes.indexOf(e.keyCode) !== -1) {
+                this.setState({currentSlideNumber: this.state.currentSlideNumber - 1});
+            }
+        }
     }
 }
 
--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,22 @@
+import * as keycode from 'keycode';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import Presentation from './Presentation';
 
+const nextKeys = [
+  keycode.codes.down,
+  keycode.codes["page down"],
+  keycode.codes.right,
+  keycode.codes.space,
+];
+const prevKeys = [
+  keycode.codes.left,
+  keycode.codes["page up"],
+  keycode.codes.up,
+  keycode.codes.backspace,
+];
+
 ReactDOM.render(
-  <Presentation />,
+  <Presentation nextKeyCodes={nextKeys} prevKeyCodes={prevKeys} />,
   document.getElementById('root') as HTMLElement
 );
